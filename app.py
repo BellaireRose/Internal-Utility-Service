fe8570224bedb6b89296005daa571f7223b938f7
BellaireRose <mmekekon@gmail.com>
2026-02-21 11:08:35 +0100

    Initialize Flask app with user routes

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from database import get_users
+import config
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Internal Utility Service Running",
+        "environment": config.ENVIRONMENT,
+        "db_host": config.DB_HOST  #leaking config info
+    })
+
+@app.route("/users")
+def users():
+    return jsonify(get_users())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)  #debug mode ON
